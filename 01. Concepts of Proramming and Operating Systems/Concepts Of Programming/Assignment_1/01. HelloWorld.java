45718befa3c006a846f37992b4798cfbd75f87cc
Toufik Pathan <130973491+toufikpathan23@users.noreply.github.com>
2023-04-22 15:38:37 +0530

    Delete 01. HelloWorld.java
    Signed-off-by: Toufik Pathan <130973491+toufikpathan23@users.noreply.github.com>

--- 01. Concepts of Proramming and Operating Systems/Concepts Of Programming/Assignment_1/01. HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package ConceptsOfProgramming.Assignment_1;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        //printing output
-        System.out.println("Hello World");
-    }
-}
